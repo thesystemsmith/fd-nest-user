7f1f66371ce2ea20d725c66b99c4a47991d78ed9
abdul rasheed <abdul.rasheed@simpleenergy.in>
2025-10-28 12:39:03 +0530

    [feat]: test success for registration of user

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,12 +2,12 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from '../dto/users/create-user.dto';
 import { UsersService } from '../services/users.service';
 
-@Controller('users')
+@Controller({ path: 'users', version: '1' })
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('register')
-  async register(@Body() createUserDto: CreateUserDto) {
+  async create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.registerUser(createUserDto);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // API prefix and versioning
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
 
   // Global validation pipe
   app.useGlobalPipes(
